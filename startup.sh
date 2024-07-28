f07ff35f4fad1f531cf97054773b91a7e04bba50
takao77 <takao.morikawa@gmail.com>
2024-07-28 11:46:45 -0400

    Convert startup.sh line endings to LF and make executable

--- startup.sh
+++ startup.sh
@@ -4,5 +4,6 @@ echo "Starting the application..."
 source antenv/bin/activate  # Activate the virtual environment
 export FLASK_APP=app_s.py
 export FLASK_ENV=production
-gunicorn --bind 0.0.0.0:80 app_s:app
+exec gunicorn --bind 0.0.0.0:80 app_s:app
+
 
